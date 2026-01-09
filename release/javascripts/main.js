2a7bccb97faa0ff3dd809e05b8bef3a66f1be4f0
Takeshi <iwatakeshi@gmail.com>
2026-01-09 12:26:09 -0700

    Refactor color processing logic into shared module
    Moved core color processing functions to a new color-processor.js module for reuse between main thread and worker. Updated main.js and worker.js to use the shared logic, reducing code duplication and improving maintainability. Updated HTML to include the new module.

--- release/javascripts/main.js
+++ release/javascripts/main.js
@@ -1,51 +1,27 @@
-import {nearestmemo, findNearestColor} from "./shared.mjs";
-
 //globals
-var nearestmemoa = nearestmemo;
+
 var currentPalette;
-var skipFirstColor = true; //skip the first color in a palette, assuming it is a transparent color
-
-//the following line is uncommented in the server-side version
-//var worker = new Worker('worker.js');
-var loading;
-console.log(worker);
-//var deltae00memo = {}; //might add a selector so that users can try the other delta E formulas
-
+var skipFirstColor = true;
+var labmemo = {};
+var nearestmemo = {};
 var dithering = false;
+var worker;
 
-//this is a hack to let Web Workers run without needing a webserver
-//https://gist.github.com/walfie/a80c4432bcff70fb826d5d28158e9cc4
-function createWebWorkerFromFunction(f) {
-  var blobContents = ['(', f.toString(), ')();'];
-  var blob = new Blob(blobContents, { type: 'application/javascript'});
-  var blobUrl = URL.createObjectURL(blob);
-  var worker = new Worker(blobUrl);
-  URL.revokeObjectURL(blobUrl);
-  return worker;
+// Initialize worker (remove the blob hack)
+if (window.Worker) {
+  worker = new Worker("javascripts/worker.js");
+
+  worker.onmessage = function (e) {
+    let canvasB = document.getElementById("image_after");
+    let ctx = canvasB.getContext("2d");
+    let imageData = ctx.getImageData(0, 0, canvasB.width, canvasB.height);
+    imageData.data.set(e.data[0]);
+    ctx.putImageData(imageData, 0, 0);
+    nearestmemo = e.data[1];
+    console.log("message received from worker");
+    document.getElementById("loading").style.display = "none";
+  };
 }
-
-function buildPath(file) {
-  var parts = document.location.href.split('/');
-  parts[parts.length - 1] = file;
-  return parts.join('/');
-}
-
-var worker = createWebWorkerFromFunction(function() {
-  self.onmessage = function(e) {
-    importScripts(...e.data.scriptUrls);
-  }
-});
-
-var workerUrl = buildPath('javascripts/worker.js');
-var includeUrl = buildPath('javascripts/deltae.global.min.js');
-var d3colorUrl = buildPath('javascripts/d3-color.js');
-var underscoreUrl = buildPath('javascripts/underscore-min.js');
-
-worker.postMessage({ scriptUrls: [
-    workerUrl, includeUrl, 
-    d3colorUrl, underscoreUrl
-] });
-//end client-side Web Worker hack
 
 function scaleImage(img) {
     let width = img.width;
@@ -65,53 +41,57 @@ function scaleImage(img) {
 }
 
 function drawImageFromFile() {
-    //this is the original image
-    let canvas = document.getElementById('image_before');
-    //the modified image appears here
-    let canvasB = document.getElementById('image_after');
-    //normal Canvas setup
-    let ctx = canvas.getContext('2d');
-    let img = new Image();
-    //this is the file, chosen by the user in the file selector
-    let curFile = document.getElementById("uploadImage").files[0];
-    let url = window.URL || window.webkitURL;
-    let src = url.createObjectURL(curFile);
-    img.src = src;
-    img.onload = function () {
+  //this is the original image
+  let canvas = document.getElementById("image_before");
+  //the modified image appears here
+  let canvasB = document.getElementById("image_after");
+  //normal Canvas setup
+  let ctx = canvas.getContext("2d");
+  let img = new Image();
+  //this is the file, chosen by the user in the file selector
+  let curFile = document.getElementById("uploadImage").files[0];
+  let url = window.URL || window.webkitURL;
+  let src = url.createObjectURL(curFile);
+  img.src = src;
+  img.onload = function () {
         let width;
         let height;
         [width, height] = scaleImage(img);
         [canvas.width, canvas.height, canvasB.width, canvasB.height] =
         [width, height, width, height]
-        //draw
-        ctx.drawImage(img, 0, 0, width, height);
-        url.revokeObjectURL(src);
-        canvasB.getContext('2d').drawImage(canvas, 0, 0); //this copies the image to the right before processing happens.  it's not necessary, but I kind of like it.
-    }
+    //draw
+    ctx.drawImage(img, 0, 0, width, height);
+    url.revokeObjectURL(src);
+    canvasB.getContext("2d").drawImage(canvas, 0, 0); //this copies the image to the right before processing happens.  it's not necessary, but I kind of like it.
+  };
 }
 
 function drawPalette() {
-    //this should be called by paletteSetup
-    //this sets up the canvas that displays the color swatches
-    let canvas = document.getElementById('palette_display');
-    let ctx = canvas.getContext('2d');
-    let width = window.screen.availWidth * 0.75;
-    canvas.width = width;
-    //if the palette is small enough, it can be displayed with nice big squares
-    let swatchSize = Math.ceil(width / currentPalette.length);
-    let rows = 1;
-    //20px is about the smallest I want one of the color-swatch squares to be
-    if (swatchSize < 20) {
-        swatchSize = 20;
-        rows = (swatchSize * currentPalette.length) / width;
-    }
-    canvas.height = Math.ceil(rows) * swatchSize;
-    //the actual drawing step
-    _.each(currentPalette, function (swatch, index) {
-        ctx.fillStyle = swatch.toString();
-        ctx.fillRect((index * swatchSize) % width, Math.floor((index * swatchSize) / width)*swatchSize, swatchSize, swatchSize);
-    })
-
+  //this should be called by paletteSetup
+  //this sets up the canvas that displays the color swatches
+  let canvas = document.getElementById("palette_display");
+  let ctx = canvas.getContext("2d");
+  let width = window.screen.availWidth * 0.75;
+  canvas.width = width;
+  //if the palette is small enough, it can be displayed with nice big squares
+  let swatchSize = Math.ceil(width / currentPalette.length);
+  let rows = 1;
+  //20px is about the smallest I want one of the color-swatch squares to be
+  if (swatchSize < 20) {
+    swatchSize = 20;
+    rows = (swatchSize * currentPalette.length) / width;
+  }
+  canvas.height = Math.ceil(rows) * swatchSize;
+  //the actual drawing step
+  _.each(currentPalette, function (swatch, index) {
+    ctx.fillStyle = swatch.toString();
+    ctx.fillRect(
+      (index * swatchSize) % width,
+      Math.floor((index * swatchSize) / width) * swatchSize,
+      swatchSize,
+      swatchSize
+    );
+  });
 }
 
 function paletteSetup() {
@@ -131,9 +111,9 @@ function paletteSetup() {
         currentPalette[i].d3color = swatch;
         currentPalette[i].lab = d3.lab(swatch);
     }
-    //empty the nearestmemo cache; it assumes the current palette
-    nearestmemoa = {};
-    drawPalette();
+  //empty the nearestmemo cache; it assumes the current palette
+  nearestmemo = {};
+  drawPalette();
 }
 
 function loadPaletteFromFile() {
@@ -148,34 +128,20 @@ function loadPaletteFromFile() {
     ]; //I worry about supporting some of these.
     if (supportedByAnypalette.includes(_.last(file.name.split('.')))) {
         AnyPalette.loadPalette(file, function (error, palette) {
-            if (palette) {
-                currentPalette = palette;
-                paletteSetup();
-            }
-            else if (error) {
-                alert(error);
-            }
-                //this would mean that AnyPalette didn't catch an error, but also didn't return a palette
-            else {
-                alert("Something has gone horribly wrong.");
-            }
-        });
-    }
-    else {
-        alert("The palette format is unsupported.");
-    }
-}
-
-function ditherHelper(img, startIdx, multiplier, error){
-    //img: Uint8ClampedArray with imageData
-    //startIdx: int, index of a red component in an image
-    //multiplier: float
-    //error: { r: int, g: int, b: int }
-    if (startIdx < img.length){
-        img[startIdx] += error.r * multiplier;
-        img[startIdx+1] += error.g * multiplier;
-        img[startIdx+2] += error.b * multiplier;
-    }
+      if (palette) {
+        currentPalette = palette;
+        paletteSetup();
+      } else if (error) {
+        alert(error);
+      }
+      //this would mean that AnyPalette didn't catch an error, but also didn't return a palette
+      else {
+        alert("Something has gone horribly wrong.");
+      }
+    });
+  } else {
+    alert("The palette format is unsupported.");
+  }
 }
 
 function swapColors() {
@@ -188,81 +154,53 @@ function swapColors() {
     //ctx.drawImage(canvasA, 0, 0);  //not necessary, but a matter of taste.  displays the original image on both sides before processing is done
     let imageData = ctx.getImageData(0, 0, canvasA.width, canvasA.height);
     
-    if (window.Worker) {
-        //this is running in its own thread and won't hang the page (yay!)
-        worker.postMessage([imageData.data, currentPalette, canvasA.width, dithering]);
-        console.log("message posted to worker");
+    if (worker) {
+    //this is running in its own thread and won't hang the page (yay!)
+    worker.postMessage([
+      imageData.data,
+      currentPalette,
+      canvasA.width,
+      dithering,
+    ]);
+    console.log("message posted to worker");
+  } else {
+    // Fallback: use shared processing function directly
+    let data = new Uint8ClampedArray(imageData.data);
 
-        //I was experimenting with a loader image, but it hasn't worked out so far
-        //document.getElementsByClassName('loader')[0].setAttribute("style", `display: block; position: absolute; left: 70%; top: 60%`)
-    }
-    else {
-        //it sucks if we get down here.  bigger image/palette combinations will take a while, and the browser will complain.
-        //this is mostly the same code as in the worker.  I could not figure out a solution besides copying it.
-        let data = new Uint8ClampedArray(imageData.data);
-        data.set(imageData.data); //real talk: I forget what this line does exactly.  it might be redundant.
-        let t0 = performance.now();
-        for (let i = 0; i < data.length; i += 4) {
-            let color = findNearestColor([data[i], data[i + 1], data[i + 2]], currentPalette).color;
-            //store the error so we can dither
-            let pixerror = {}; 
-            pixerror.r = data[i] - color.r;
-            pixerror.g = data[i + 1] - color.g;
-            pixerror.b = data[i + 2] - color.b;
-        
-            //this is just setting the pixel to whatever color
-            data[i] = color.r;
-            data[i + 1] = color.g;
-            data[i + 2] = color.b;
-            
-            if (dithering){
-                //need to adjust the pixel to the right of this one, and the three pixels below it
-                //(* is pixel to change, . is this pixel)
-                //  . *
-                //* * *
-                //make sure this pixel is really on the right
-                if ( Math.floor((i+4)/width) == Math.floor(i/width ) ){
-                    ditherHelper(data, i+4, 7/16, pixerror);
-                }
-    
-                let below = i + width;
-                ditherHelper(data, below, 5/16, pixerror);
-    
-                if ( Math.floor(below/width) == Math.floor((below-4)/width) ){
-                    ditherHelper(data, below-4, 3/16, pixerror);
-                }
-                
-                if ( Math.floor(below/width) == Math.floor((below+4)/width) ){
-                    ditherHelper(data, below+4, 1/16, pixerror);
-                }
-            }
-        }
-        document.getElementById("loading").style.display = "none";
-        let t1 = performance.now();
-        console.log(`Time to swap colors of whole image: ${t1 - t0} ms`); //probably a lot
-        ctx = canvasB.getContext('2d');
-        let imageDataB = ctx.getImageData(0, 0, canvasB.width, canvasB.height);
-        imageDataB.data.set(data);
-        ctx.putImageData(imageDataB, 0, 0);
-    }
-}
+    let t0 = performance.now();
 
-worker.onmessage = function (e) {
-    let canvasB = document.getElementById('image_after');
-    let ctx = canvasB.getContext('2d');
-    let imageData = ctx.getImageData(0, 0, canvasB.width, canvasB.height);
-    imageData.data.set(e.data[0]);
-    ctx.putImageData(imageData, 0, 0);
-     //since the Web Worker doesn't have access to nearestmemo, its copied back and forth between the threads
-    nearestmemoa = e.data[1];
-    //document.getElementsByClassName('loader')[0].setAttribute("style", "display: none");
-    console.log("message received from worker");
+    // Call shared processing function (no duplication!)
+    let result = processImageData(
+      data,
+      currentPalette,
+      canvasA.width,
+      dithering,
+      nearestmemo,
+      labmemo
+    );
+
+    let t1 = performance.now();
+    console.log(`Time to swap colors: ${t1 - t0} ms`);
+
+    ctx = canvasB.getContext("2d");
+    let imageDataB = ctx.getImageData(0, 0, canvasB.width, canvasB.height);
+    imageDataB.data.set(result.data);
+    ctx.putImageData(imageDataB, 0, 0);
+
+    nearestmemo = result.nearestmemo;
     document.getElementById("loading").style.display = "none";
+  }
 }
 
+//not sure yet if these are the functions I want bound to these listeners.  but I guess that can wait.
 window.onload = function () {
-    document.getElementById("uploadImage").addEventListener("change", drawImageFromFile, false);
-    document.getElementById("uploadPalette").addEventListener("change", loadPaletteFromFile, false);
-    document.getElementsByTagName("button")[0].addEventListener("click", swapColors, false);
-}
-export {loadPaletteFromFile}
+  document
+    .getElementById("uploadImage")
+    .addEventListener("change", drawImageFromFile, false);
+  document
+    .getElementById("uploadPalette")
+    .addEventListener("change", loadPaletteFromFile, false);
+  document
+    .getElementsByTagName("button")[0]
+    .addEventListener("click", swapColors, false);
+};
--- release/javascripts/worker.js
+++ release/javascripts/worker.js
@@ -1,104 +1,33 @@
-//importScripts('https://d3js.org/d3-color.v1.min.js', 'https://underscorejs.org/underscore-min.js');
+importScripts("https://d3js.org/d3-color.v1.min.js");
+importScripts("https://underscorejs.org/underscore-min.js");
+importScripts("deltae.global.min.js");
+importScripts("color-processor.js"); // Import shared logic
 
-//still trying to figure out how to replace code with a module
-
-//import("./shared.mjs");
 var nearestmemo = {};
 var labmemo = {};
 
-function d3lab(r, g, b) {
-    //r,g,b are integers from 0 to 255, as from ImageData
-    let color = r << 16 | g << 8 | b;
-    if (labmemo[color]) {
-        return labmemo[color];
-    }
-    else {
-        labmemo[color] = d3.lab(d3.rgb(r, g, b));
-        return labmemo[color];
-    }
-}
+self.onmessage = function (e) {
+  let [imageData, palette, width, dithering] = e.data;
+  let data = new Uint8ClampedArray(imageData);
 
-function findNearestColor(pixel, palette, formula=DeltaE.getDeltaE00) {
-    //pixel: an array with three components
-    //palette: an array of objects
-    //this assumes the anypalette format, where each object contains properties for r, g, and b
-    //and also a toString method which returns a CSS Color Module Level 3 specifier string
-    //e.g., "rgb(255, 255, 255)"
-    let key = pixel[0] << 16 | pixel[1] << 8 | pixel[2];
-    if (nearestmemo[key]) {
-        return nearestmemo[key];
-    }
-    let deltaEs = palette.map(function (color) {
-        //color is the object
-        let swatch = color.d3color;
-        let labSwatch = color.lab;
-        let labPixel = d3lab(pixel[0], pixel[1], pixel[2]);
-        let obj = {
-            color: swatch,
-            deltaE: formula(
-                { L: labSwatch.l, A: labSwatch.a, B: labSwatch.b }, { L: labPixel.l, A: labPixel.a, B: labPixel.b })
-        };
-        return obj;
-    });
-    let result = _.min(deltaEs, _.iteratee('deltaE'));
-    nearestmemo[key] = result;
-    return result;
-}
+  let t0 = performance.now();
 
+  // Use shared processing function
+  let result = processImageData(
+    data,
+    palette,
+    width,
+    dithering,
+    nearestmemo,
+    labmemo
+  );
 
-function ditherHelper(img, startIdx, multiplier, error){
-    //img: Uint8ClampedArray with imageData
-    //startIdx: int, index of a red component in an image
-    //multiplier: float
-    //error: { r: int, g: int, b: int }
-    if (startIdx < img.length){
-        img[startIdx] += (error.r * multiplier);
-        img[startIdx+1] += (error.g * multiplier);
-        img[startIdx+2] += (error.b * multiplier);
-    }
-}
+  let t1 = performance.now();
+  console.log(`Worker processed image in ${t1 - t0} ms`);
 
-onmessage = function (e) {
-    let data = new Uint8ClampedArray(e.data[0]);
-    data.set(e.data[0]);
-    let width = e.data[2];
-    let dithering = e.data[3];
-    for (let i = 0; i < data.length; i += 4) {
-        let color = findNearestColor([data[i], data[i + 1], data[i + 2]], e.data[1]).color;
-        //store the error so we can dither
-        let pixerror = {};
-        pixerror.r = data[i] - color.r;
-        pixerror.g = data[i + 1] - color.g;
-        pixerror.b = data[i + 2] - color.b;
+  // Send back processed data and updated cache
+  self.postMessage([result.data, result.nearestmemo]);
 
-        //this is just setting the pixel to whatever color
-        data[i] = color.r;
-        data[i + 1] = color.g;
-        data[i + 2] = color.b;
-        
-
-        if (dithering){
-            //need to adjust the pixel to the right of this one, and the three pixels below it
-            //(* is pixel to change, . is this pixel)
-            //  . *
-            //* * *
-
-            //check if this pixel is really on the right
-            if ( Math.floor((i+4)/width) == Math.floor(i/width ) ){
-                ditherHelper(data, i+4, 7/16, pixerror);
-            }
-
-            let below = i + width;
-            ditherHelper(data, below, 5/16, pixerror);
-
-            if ( Math.floor(below/width) == Math.floor((below-4)/width) ){
-                ditherHelper(data, below-4, 3/16, pixerror);
-            }
-            
-            if ( Math.floor(below/width) == Math.floor((below+4)/width) ){
-                ditherHelper(data, below+4, 1/16, pixerror);
-            }
-        }
-    }
-    postMessage([data, nearestmemo]);
-}
+  // Update worker's cache for next run
+  nearestmemo = result.nearestmemo;
+};
