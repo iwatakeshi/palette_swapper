f310b8cb9236b47580c540a4ec93a05bac5a5a8e
Takeshi <iwatakeshi@gmail.com>
2026-01-09 12:48:39 -0700

    Refactor main.js for improved readability and consistency
    Reformatted code for consistent indentation and style, making the logic easier to follow. No functional changes were made; this is a style and readability update.

--- release/javascripts/main.js
+++ release/javascripts/main.js
@@ -24,20 +24,20 @@ if (window.Worker) {
 }
 
 function scaleImage(img) {
-    let width = img.width;
-    let height = img.height;
-    let ratio;
-    if (img.height > window.screen.availHeight) {
-        height = window.screen.availHeight - (window.screen.availHeight*0.2);
-        ratio = height / img.height;
-        width = ratio * img.width;
-    }
-    else if (img.width > (window.screen.availWidth / 2)) {
-        width = Math.floor(window.screen.availWidth / 2) - (window.screen.availWidth * 0.1);
-        ratio = width / img.width;
-        height = ratio * img.height;
-    }
-    return [width, height]
+  let width = img.width;
+  let height = img.height;
+  let ratio;
+  if (img.height > window.screen.availHeight) {
+    height = window.screen.availHeight - window.screen.availHeight * 0.2;
+    ratio = height / img.height;
+    width = ratio * img.width;
+  } else if (img.width > window.screen.availWidth / 2) {
+    width =
+      Math.floor(window.screen.availWidth / 2) - window.screen.availWidth * 0.1;
+    ratio = width / img.width;
+    height = ratio * img.height;
+  }
+  return [width, height];
 }
 
 function drawImageFromFile() {
@@ -54,11 +54,15 @@ function drawImageFromFile() {
   let src = url.createObjectURL(curFile);
   img.src = src;
   img.onload = function () {
-        let width;
-        let height;
-        [width, height] = scaleImage(img);
-        [canvas.width, canvas.height, canvasB.width, canvasB.height] =
-        [width, height, width, height]
+    let width;
+    let height;
+    [width, height] = scaleImage(img);
+    [canvas.width, canvas.height, canvasB.width, canvasB.height] = [
+      width,
+      height,
+      width,
+      height,
+    ];
     //draw
     ctx.drawImage(img, 0, 0, width, height);
     url.revokeObjectURL(src);
@@ -95,39 +99,56 @@ function drawPalette() {
 }
 
 function paletteSetup() {
-    currentPalette = AnyPalette.uniqueColors(currentPalette);
-    //this removes black, which is often included at the beginning of palettes for some reason
-    if (skipFirstColor) {
-        currentPalette = _.rest(currentPalette);
-    }
-    //pre-calculate the Lab color for each (rgb) color in the palette
-    for (var i = 0; i < currentPalette.length; ++i) {
-        let colorString = currentPalette[i].toString();
-        //this is added to fix long floating point stuff,
-        //like "rgb(169.00000512599945, 59.00000028312206, 59.00000028312206)"
-        const re = /\.[0-9]+/g;
-        colorString = colorString.replaceAll(re, "")
-        let swatch = d3.color(colorString);
-        currentPalette[i].d3color = swatch;
-        currentPalette[i].lab = d3.lab(swatch);
-    }
+  currentPalette = AnyPalette.uniqueColors(currentPalette);
+  //this removes black, which is often included at the beginning of palettes for some reason
+  if (skipFirstColor) {
+    currentPalette = _.rest(currentPalette);
+  }
+  //pre-calculate the Lab color for each (rgb) color in the palette
+  for (var i = 0; i < currentPalette.length; ++i) {
+    let colorString = currentPalette[i].toString();
+    //this is added to fix long floating point stuff,
+    //like "rgb(169.00000512599945, 59.00000028312206, 59.00000028312206)"
+    const re = /\.[0-9]+/g;
+    colorString = colorString.replaceAll(re, "");
+    let swatch = d3.color(colorString);
+    currentPalette[i].d3color = swatch;
+    currentPalette[i].lab = d3.lab(swatch);
+  }
   //empty the nearestmemo cache; it assumes the current palette
   nearestmemo = {};
   drawPalette();
 }
 
 function loadPaletteFromFile() {
-    //this is pretty similar to drawImageFromFile, but with palettes
-    let file = document.getElementById("uploadPalette").files[0];
-    //only the file extension is checked; i.e., it might be a valid file but need renaming
-    let supportedByAnypalette = ['pal', 'gpl', 'aco', 'ase',
-         'txt','psppalette', 'hpl', 'cs', 'wpe', 'sketchpalette',
-         'spl', 'soc', 'colors', 'theme', 'themepack',
-         'css', 'scss', 'styl',
-         'html', 'svg', 'js'
-    ]; //I worry about supporting some of these.
-    if (supportedByAnypalette.includes(_.last(file.name.split('.')))) {
-        AnyPalette.loadPalette(file, function (error, palette) {
+  //this is pretty similar to drawImageFromFile, but with palettes
+  let file = document.getElementById("uploadPalette").files[0];
+  //only the file extension is checked; i.e., it might be a valid file but need renaming
+  let supportedByAnypalette = [
+    "pal",
+    "gpl",
+    "aco",
+    "ase",
+    "txt",
+    "psppalette",
+    "hpl",
+    "cs",
+    "wpe",
+    "sketchpalette",
+    "spl",
+    "soc",
+    "colors",
+    "theme",
+    "themepack",
+    "css",
+    "scss",
+    "styl",
+    "html",
+    "svg",
+    "js",
+  ]; //I worry about supporting some of these.
+  if (supportedByAnypalette.includes(_.last(file.name.split(".")))) {
+    AnyPalette.loadPalette(file, function (error, palette) {
       if (palette) {
         currentPalette = palette;
         paletteSetup();
@@ -145,16 +166,16 @@ function loadPaletteFromFile() {
 }
 
 function swapColors() {
-    document.getElementById("loading").style.display = "block";
-    let canvasA = document.getElementById('image_before');
-    let canvasB = document.getElementById('image_after');
-	//we're always reading from the "before" image and writing to the "after" image
-    let ctx = canvasA.getContext('2d');
+  document.getElementById("loading").style.display = "block";
+  let canvasA = document.getElementById("image_before");
+  let canvasB = document.getElementById("image_after");
+  //we're always reading from the "before" image and writing to the "after" image
+  let ctx = canvasA.getContext("2d");
 
-    //ctx.drawImage(canvasA, 0, 0);  //not necessary, but a matter of taste.  displays the original image on both sides before processing is done
-    let imageData = ctx.getImageData(0, 0, canvasA.width, canvasA.height);
-    
-    if (worker) {
+  //ctx.drawImage(canvasA, 0, 0);  //not necessary, but a matter of taste.  displays the original image on both sides before processing is done
+  let imageData = ctx.getImageData(0, 0, canvasA.width, canvasA.height);
+
+  if (worker) {
     //this is running in its own thread and won't hang the page (yay!)
     worker.postMessage([
       imageData.data,
